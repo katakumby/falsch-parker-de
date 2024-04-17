18993a68dafda67ec9ab6407056790bef472b22c
fuhrmann.software <fuhrmann@fuhrmann.software>
2024-04-17 16:30:52 +0300

    added locations and about us metadata

--- src/app/[lang]/terms-and-conditions/page.jsx
+++ src/app/[lang]/terms-and-conditions/page.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container } from '@/components/container/container';
 import { TermsAndConditionsArticle } from '@/app/[lang]/terms-and-conditions/components/termsAndConditionsArticle';
+import { buildMetadata } from '@/utils/metadata';
+
+export const metadata = buildMetadata('termsAndConditions');
 
 const TermsAndConditionsPage = () => {
   return (
