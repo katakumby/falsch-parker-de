fce3e592e0304d58456bbdaba942831ec32b7a14
fuhrmann.software <fuhrmann@fuhrmann.software>
2024-04-20 14:26:19 +0300

    Redirect for old site pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/abschleppdienst',
+        destination: '/de/towing',
+        permanent: true,
+      },
+      {
+        source: '/umtriebsentschadigung',
+        destination: '/de/parking-ticket',
+        permanent: true,
+      },
+      {
+        source: '/kontakt',
+        destination: '/de/contact',
+        permanent: true,
+      },
+      {
+        source: '/agb',
+        destination: '/de/terms-and-conditions',
+        permanent: true,
+      },
+      {
+        source: '/impressum',
+        destination: '/de/imprint',
+        permanent: true,
+      },
+      {
+        source: '/datenschutzerklaerung',
+        destination: '/de/data-protection',
+        permanent: true,
+      },
+      {
+        source: '/abschleppdienst/zurich',
+        destination: '/de/location/zurich',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { hostname: 'cdn.sanity.io' },
